77f8b9e19eaca3e50daa450bcc9f259bb2406049
tratsc <boldyrev.danyilaaa@gmail.com>
2024-10-10 09:20:08 +0300

    Update Quick sorting.js

--- Quick sorting.js
+++ Quick sorting.js
@@ -1,10 +1,6 @@
 // находит в масиве наибольшое число
 var Bubble_sorting = (arr) => {
     var len = arr.length - 1
-    var index = 0 
-    var a 
-    var b 
-    var c 
     for (var i = 0;i <= len;i++) {
         for (var j = i+1;arr[i] <= arr[j];j++) {
             [arr[i], arr[j]] = [arr[j], arr[i]]
